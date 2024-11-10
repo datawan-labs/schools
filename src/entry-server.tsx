10998ca8c8a5107523b75692f55ef30080236bda
Jafar Aziz <aziz@jafaraziz.com>
2024-11-11 00:20:57 +0700

    fix: move opengraph meta to entry-server

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,6 +9,44 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.svg" />
+          <title>Mapping Education in Indonesia | Datawan</title>
+          <meta
+            name="description"
+            content="Mapping Education: Visualizing School Distribution and Population Density in Indonesia"
+          />
+
+          {/* opengraph */}
+          <meta
+            property="og:title"
+            content="Mapping Education in Indonesia | Datawan"
+          />
+          <meta
+            property="og:description"
+            content="Mapping Education: Visualizing School Distribution and Population Density in Indonesia"
+          />
+          <meta
+            property="og:image"
+            content="https://schools.datawan.id/og.png"
+          />
+          <meta property="og:url" content="https://schools.datawan.id" />
+
+          {/* twitter / x */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@jfrAziz" />
+          <meta
+            name="twitter:image"
+            content="https://schools.datawan.id/og.png"
+          />
+          <meta
+            property="twitter:title"
+            content="Mapping Education in Indonesia | Datawan"
+          />
+          <meta
+            property="twitter:description"
+            content="Mapping Education: Visualizing School Distribution and Population Density in Indonesia"
+          />
+
+          {/* assets */}
           {assets}
         </head>
         <body data-kb-theme="dark">
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { config } from "@/stores";
-import { Title, Meta } from "@solidjs/meta";
 import { Widget } from "@/components/widget";
 import { Layers } from "@/components/layers";
 import { MapInstance, MapProvider } from "@/components/ui/maps";
@@ -8,38 +7,11 @@ import { LayersTooltip } from "@/components/widget/layers-tooltip";
 export default function Home() {
   return (
     <main class="relative h-svh w-full overflow-hidden">
-      <Title>Mapping Education in Indonesia | Datawan</Title>
-      <Meta
-        property="og:title"
-        content="Mapping Education in Indonesia | Datawan"
-      />
-      <Meta
-        property="og:description"
-        content="Mapping Education: Visualizing School Distribution and Population Density in Indonesia"
-      />
-      <Meta property="og:image" content="https://schools.datawan.id/og.png" />
-      <Meta property="og:url" content="https://schools.datawan.id" />
-      <Meta name="twitter:card" content="summary_large_image" />
-      <Meta name="twitter:site" content="@jfrAziz" />
-      <Meta name="twitter:image" content="https://schools.datawan.id/og.png" />
-      <Meta
-        property="twitter:title"
-        content="Mapping Education in Indonesia | Datawan"
-      />
-      <Meta
-        property="twitter:description"
-        content="Mapping Education: Visualizing School Distribution and Population Density in Indonesia"
-      />
-
       <MapProvider>
         <MapInstance
-          attributionControl={false}
           mapStyle={config.styles.styles}
           class="absolute top-0 left-0 size-full"
-          mapView={{
-            center: [106.847021, -6.248413],
-            zoom: 6,
-          }}
+          mapView={{ center: [106.847021, -6.248413], zoom: 6 }}
         />
         <Widget />
         <LayersTooltip />
